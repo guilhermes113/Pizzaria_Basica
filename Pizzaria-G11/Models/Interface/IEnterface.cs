e63d22743cb6fda3a18a1e1f8b9c465dec3050ce
GUILHERME SANCHES DA ROCHA <guilherme.srocha11@senacsp.edu.br>
2022-08-11 19:58:58 -0300

    Iniciando Projeto

---
+++ Pizzaria-G11/Models/Interface/IEnterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzaria_G11.Models.Interface
+{
+    public class IEnterface
+    {
+        public int Id { get; }
+        public DateTime DataCadastro { get; }
+        public DateTime DataAlteração { get; }
+    }
+}
---
+++ Pizzaria-G11/Models/Pizza.cs
@@ -0,0 +1,13 @@
+﻿using Pizzaria_G11.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzaria_G11.Models
+{
+    public class Pizza : IEnterface
+    {
+            public int Id { get;  }//dd
+    }
+}
